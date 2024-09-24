b399df4ad144ce08340f5a2c1c798f8f817cdb9e
Isabelle Eloise Rugno Ferreira <Isabelle.eloise4369@gmail.com>
2024-09-24 10:44:55 -0300

    Change: Visual dos botões adicionando a temática de Jojo Bizarre Adventures (Em breve será reformulado para melhor legibilidade dos textos).

--- frmPrincipal.cs
+++ frmPrincipal.cs
@@ -12,6 +12,8 @@ namespace Calculadora
 {
     public partial class frmPrincipal : Form
     {
+
+        readonly DataTable tabela = new DataTable();
         string equacao = "";
 
         public frmPrincipal()
@@ -22,12 +24,57 @@ namespace Calculadora
         private void btnApagar_Click(object sender, EventArgs e)
         {
             lblResultado.Text = "";
+
             this.equacao = "";
         }
 
         private void btnIgual_Click(object sender, EventArgs e)
         {
-            lblResultado.Text = Math.Equals();
+            this.equacao = this.tabela.Compute(this.equacao, null).ToString()!;
+
+            lblResultado.Text = this.equacao;
+        }
+
+        private void UpdateEquacao(string txt)
+        {
+            this.equacao += txt;
+
+            lblResultado.Text = this.equacao;
+        }
+        private void btnSoma_Click(object sender, EventArgs e)
+        {
+            UpdateEquacao("+");
+        }
+
+        private void btnSubtracao_Click(object sender, EventArgs e)
+        {
+            UpdateEquacao("-");
+        }
+
+        private void btnMultiplicacao_Click(object sender, EventArgs e)
+        {
+            UpdateEquacao("*");
+        }
+
+        private void btnDivisao_Click(object sender, EventArgs e)
+        {
+            UpdateEquacao("/");
+        }
+
+        private void btnTrocaDeSinal_Click(object sender, EventArgs e)
+        {
+            UpdateEquacao("* -1");
+        }
+
+        private void btnZero_Click(object sender, EventArgs e)
+        {
+            UpdateEquacao("0");
+        }
+
+        private void frmPrincipal_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // Fecha todos os processos do Aplicativo
+            Application.Exit();
         }
     }
 }
